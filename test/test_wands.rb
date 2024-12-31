0aa243842ba08c600f86930cd3feed8f86f7d8c1
shigeru.nakajima <shigeru.nakajima@gmail.com>
2024-12-31 15:38:58 +0900

    Add test for sending and receiving message

--- test/test_wands.rb
+++ test/test_wands.rb
@@ -8,13 +8,16 @@ class TestWands < Minitest::Test
     refute_nil ::Wands::VERSION
   end
 
-  def test_open_connection
+  def test_send_and_receive_message
     Async do |task|
       task.async do
         server = Wands::WebSocketServer.open("localhost", 23_456)
         socket = server.accept
 
         assert_instance_of Wands::WebSocket, socket
+        assert_equal "Hello, World!", socket.gets
+
+        socket.write("Goodbye, World!")
         socket.close
       end
 
@@ -22,6 +25,9 @@ class TestWands < Minitest::Test
         socket = Wands::WebSocket.open("localhost", 23_456)
 
         assert_instance_of Wands::WebSocket, socket
+        socket.write("Hello, World!")
+
+        assert_equal "Goodbye, World!", socket.gets
         socket.close
       end
     end
