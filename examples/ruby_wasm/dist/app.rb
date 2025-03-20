4ed0b4b510413402bf12d6ad4f7959af5f6c1d73
shigeru.nakajima <shigeru.nakajima@gmail.com>
2025-03-20 17:51:39 +0900

    send and receive multi messages

--- examples/ruby_wasm/dist/app.rb
+++ examples/ruby_wasm/dist/app.rb
@@ -3,7 +3,10 @@ require "wands/web_socket"
 
 socket = Wands::WebSocket.open("localhost", 23456)
 
-p 'open'
 socket.puts('Hello World!')
+
+p "receive  #{socket.gets}"
+
+socket.puts('Goodbye World!')
 
 p "receive  #{socket.gets}"
