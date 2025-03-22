5fc91e0a1f8217b624db29b0c5b971655d90676a
shigeru.nakajima <shigeru.nakajima@gmail.com>
2025-03-22 15:58:07 +0900

    Fix style

--- node_wasi_test/tests/test_unit.rb
+++ node_wasi_test/tests/test_unit.rb
@@ -1,1 +1,3 @@
-require_relative "./unit/test_queue"
+# frozen_string_literal: true
+
+require_relative "unit/test_queue"
--- node_wasi_test/tests/unit/test_queue.rb
+++ node_wasi_test/tests/unit/test_queue.rb
@@ -1,20 +1,23 @@
+# frozen_string_literal: true
+
 require "test-unit"
 require "js"
 require "wands/js/queue"
 
-class Wands::TestQueue < Test::Unit::TestCase
-  def test_push_and_pop
-    queue = Wands::JS::Queue.new
-    queue.push("Hello World!")
-    assert_equal "Hello World!", queue.pop
-  end
-
-  def test_wait_once
-
-    start = Time.now
-    Wands::JS::Queue.wait_once do |queue|
-      JS.global.setTimeout(-> { queue.push("Hello World!") }, 100)
+module Wands
+  class TestQueue < Test::Unit::TestCase
+    def test_push_and_pop
+      queue = Wands::JS::Queue.new
+      queue.push("Hello World!")
+      assert_equal "Hello World!", queue.pop
     end
-    assert_in_delta 0.1, Time.now - start, 0.05
+
+    def test_wait_once
+      start = Time.now
+      Wands::JS::Queue.wait_once do |queue|
+        JS.global.setTimeout(-> { queue.push("Hello World!") }, 100)
+      end
+      assert_in_delta 0.1, Time.now - start, 0.05
+    end
   end
 end
